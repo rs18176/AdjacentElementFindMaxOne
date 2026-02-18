6a573ad9989b970a063d43dd92777d79c6c4cecb
unknown <rs18176@gmail.com>
2026-02-18 21:43:59 +0530

    remove adjacent element from array and new element by adding one value in the same element and find the Max element from the array

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[5] { 5, 3, 3, 3, 4};
+        ArrayList list = new ArrayList();
+        foreach (int i in arr)
+        {
+            list.Add(i);
+            list.Sort();
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = 0; j < list.Count; j++)
+            {
+                if (i != j && ((int)list[i]).Equals((int)list[j]))
+                {
+                    int temp = (int)list[i] + 1;
+                    list.Add(temp);
+                    list.Remove(list[i]);
+                    list.Remove(list[i]);
+                    list.Sort();
+                    i = -1;
+                    break;
+                }
+            }
+        }
+        int max = list.Cast<int>().Max();
+        Console.WriteLine(max);
+        Console.ReadLine();
+    }
+}
